e7d02c8d502677542cf78db7f25e78b30c8801c1
GAO Yichao <gaoyichao2013@126.com>
2017-08-08 03:17:45 +0800

    定时触发的上下文切换

--- pro/Particle/app/main.c
+++ pro/Particle/app/main.c
@@ -2,10 +2,6 @@
 #include <xtos.h>
 #include <led.h>
 
-void delay(int c) {
-    for (int i = 0; i < c; i++);
-}
-
 #define TASKA_STK_SIZE 1024
 #define TASKB_STK_SIZE 1024
 static uint32 taskA_Stk[TASKA_STK_SIZE];
@@ -26,30 +22,46 @@ void task_switch() {
 void taska() {
     while (1) {
         led_set_color(100, 0, 5);
-        for (int i = 0; i < 1000; i++)
-            delay(10000);
-        task_switch();
     }
 }
 
 void taskb() {
     while (1) {
         led_set_color(5, 0, 100);
-        for (int i = 0; i < 1000; i++)
-            delay(10000);
+    }
+}
+
+#define SysTicks_Irq_n 15
+
+void systick_init(uint32 ticks) {
+    SYSTICK->LOAD.bits.cnt = ticks;         // 重装载数据
+    SYSTICK->VAL.bits.cnt = 0;              // 清除当前计数
+
+    SCB->SHP[SysTicks_Irq_n - 4] = 0x00;    // 暂时赋予最高优先级
+
+    SYSTICK->CTRL.bits.clksource = 1;       // 使用CPU时钟168MHz
+    SYSTICK->CTRL.bits.tickint = 1;         // 开启计数溢出中断
+    SYSTICK->CTRL.bits.en = 0;              // 暂不开启计时器
+}
+
+void SysTick_Handler(void) {
+    static int counter = 0;
+
+    if ((counter++ % 1000) == 0) {
         task_switch();
     }
 }
 
-
 int main(void) {
     led_pwm_init();
+    systick_init(168000);
 
     xtos_create_task(&taskA, taska, &taskA_Stk[TASKA_STK_SIZE - 1]);
     xtos_create_task(&taskB, taskb, &taskB_Stk[TASKB_STK_SIZE - 1]);
 
     gp_xtos_next_task = &taskA;
 
+    SYSTICK->CTRL.bits.en = 1;
     xtos_start();
 
     while (1) {
