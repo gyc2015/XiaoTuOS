ae1ffa71fa6e4e719c12c047e0fa7fa48d0ac487
GAO Yichao <gaoyichao2013@126.com>
2017-08-08 02:00:03 +0800

    上下文切换控制三色灯

--- pro/Particle/app/main.c
+++ pro/Particle/app/main.c
@@ -1,49 +1,58 @@
 #include <stm32f407.h>
 #include <xtos.h>
-#include <power.h>
 #include <led.h>
-#include <w5500.h>
 
-void config_interruts(void);
+void delay(int c) {
+    for (int i = 0; i < c; i++);
+}
 
-extern struct w5500 gW5500;
-uint8 remote_ip[4] = { 192,168,1,101 };
-uint8 gTmpByte = 0;
-uint16 gReceivedLen = 0;
-uint8 gRxBuf[1024];
-w5500_error_t gW5500Err = Err_W5500_No_Error;
+#define TASKA_STK_SIZE 1024
+#define TASKB_STK_SIZE 1024
+static uint32 taskA_Stk[TASKA_STK_SIZE];
+static uint32 taskB_Stk[TASKB_STK_SIZE];
+
+static struct xtos_task_struct taskA;
+static struct xtos_task_struct taskB;
+
+void task_switch() {
+    if (gp_xtos_cur_task == &taskA)
+        gp_xtos_next_task = &taskB;
+    else
+        gp_xtos_next_task = &taskA;
+
+    xtos_context_switch();
+}
+
+void taska() {
+    while (1) {
+        led_set_color(100, 0, 5);
+        for (int i = 0; i < 1000; i++)
+            delay(10000);
+        task_switch();
+    }
+}
+
+void taskb() {
+    while (1) {
+        led_set_color(5, 0, 100);
+        for (int i = 0; i < 1000; i++)
+            delay(10000);
+        task_switch();
+    }
+}
+
 
 int main(void) {
     led_pwm_init();
 
-    for (int i = 0; i < 5000; i++)
-        for (int j = 0; j < 1000; j++)
-            ;
+    xtos_create_task(&taskA, taska, &taskA_Stk[TASKA_STK_SIZE - 1]);
+    xtos_create_task(&taskB, taskb, &taskB_Stk[TASKB_STK_SIZE - 1]);
 
-    config_interruts();
+    gp_xtos_next_task = &taskA;
 
-    led_set_color(0, 0, 5);
-
-    w5500_init(&gW5500);
+    xtos_start();
 
     while (1) {
-        switch (w5500_read_byte(W5500_Sn_SR(0))) {
-        case W5500_SnSR_CLOSED:
-            gW5500Err = w5500_init_socket(0, W5500_SnMR_TCP | W5500_SnMR_ND, 0);
-            break;
-        case W5500_SnSR_INIT:
-            gW5500Err = w5500_connect_socket(0, remote_ip, 60001);
-            break;
-        case W5500_SnSR_ESTABLISHED:
-            gReceivedLen = w5500_get_socket_received_len(0);
-            if (gReceivedLen > 0) {
-                gW5500Err = w5500_receive_socket(0, gRxBuf, gReceivedLen);
-                gW5500Err = w5500_send_socket(0, gRxBuf, gReceivedLen);
-            }
-            break;
-        default:
-            gTmpByte = w5500_read_byte(W5500_Sn_SR(0));
-            break;
-        }
+
     }
 }
